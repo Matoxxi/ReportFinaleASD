df28c7609a003f43412be0c9d932572d49afdc50
Matteo Asuni <asunimatteo@gmail.com>
2019-05-31 19:15:42 +0200

    schifo di clion

---
+++ cmake-build-debug/CMakeFiles/ReportFinaleASD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReportFinaleASD.dir/main.c.obj"
+  "ReportFinaleASD.pdb"
+  "ReportFinaleASD.exe"
+  "ReportFinaleASD.exe.manifest"
+  "libReportFinaleASD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ReportFinaleASD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
