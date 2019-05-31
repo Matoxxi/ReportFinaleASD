ed0a2ce0af8ef547e3af3aa636e10dcdaae7b107
Matteo Asuni <asunimatteo@gmail.com>
2019-05-31 19:30:18 +0200

    Generazione array, con caso heap

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define MANUALE 0
 #define AUTOMATICA 1
 #define NUM_DIMENSIONI 7
@@ -6,7 +8,10 @@
 #define NUM_ALGORITMI 5
 typedef enum{ORDINATO, QUASI_ORDINATO, INV_ORDINATO, CASUALE} inputType;
 typedef enum{SelectionSort,InsertionSort,HeapSort,QuickSort,MergeSort} algoritmi;
+int generaCasuale(int min, int max);
+int *genera_array(int dim,inputType tipo_schema,int flag);
 int main() {
+    srand(time(NULL));
     int scelta,dim;
     int dimensioni[NUM_DIMENSIONI]={500,1000,2000,5000,10000,20000,50000};
     char nomiTipologie[NUM_TIPOLOGIE][30]={"Ordinato","Quasi ordinato","Inversamente ordinato","Casuale"};
@@ -36,7 +41,6 @@ int main() {
                 }
                 scanf("%d",&tipologia);
             }while(tipologia<ORDINATO || tipologia>CASUALE);
-            printf("Hai scelto di creare un array di %d elementi con lo schema %s\n",dim,nomiTipologie[tipologia]);
             do{
                 printf("Scegli l'algoritmo con cui vuoi ordinare l'array\n");
                 for(i=0;i<NUM_ALGORITMI;i++){
@@ -44,6 +48,19 @@ int main() {
                 }
                 scanf("%d",&algoritmo);
             }while(algoritmo<SelectionSort || algoritmo>MergeSort);
+            printf("Hai scelto di creare un array di %d elementi con lo schema %s, che verra' ordinato"
+                   " con l'algoritmo %s \n",dimensioni[dim],nomiTipologie[tipologia],nomiAlgoritmi[algoritmo]);
+            if(algoritmo==2) {
+                v = genera_array(dimensioni[dim], tipologia, 1);
+
+            }
+            else {
+                v = genera_array(dimensioni[dim], tipologia, 0);
+                for(i=0;i<dimensioni[dim];i++){
+                    printf("%d ",v[i]);
+                }
+                printf("\n");
+            }
              break;
         case AUTOMATICA:
             break;
@@ -51,4 +68,90 @@ int main() {
             break;
     }
     return 0;
+}
+
+
+int *genera_array(int dim,inputType tipo_schema,int flag){
+    int *v,i,j;
+    switch (flag){
+        case 0:
+            v = (int *) malloc(sizeof(int) * dim);
+            if(v==NULL)
+                exit(-1);
+            switch(tipo_schema){
+                case ORDINATO:
+
+                    for(i=0;i<dim;i++){
+                        v[i]=i+1;
+                    }
+                    break;
+                case INV_ORDINATO:
+                    for(i=0;i<dim;i++){
+                        v[i]=dim-i;
+                    }
+                    break;
+                case CASUALE:
+                    for(i=0;i<dim;i++){
+                        v[i]=generaCasuale(1,dim);
+                    }
+                    break;
+                case QUASI_ORDINATO:
+                    for(i=0;i<dim/2;i++){
+                        v[i]=i+1;
+                    }
+                    for(j=i;j<dim;j++){
+                        v[j]=generaCasuale(10,20);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case 1:
+            v = (int *) malloc(sizeof(int) * dim+1);
+            if(v==NULL)
+                exit(-1);
+            switch(tipo_schema){
+                case ORDINATO:
+                    v[0]=0;
+                    for(i=1;i<dim+1;i++){
+                        v[i]=i-1;
+                    }
+                    break;
+                case INV_ORDINATO:
+                    v[0]=0;
+                    for(i=1;i<dim+1;i++){
+                        v[i]=dim-i;
+                    }
+                    break;
+                case CASUALE:
+                    v[0]=0;
+                    for(i=1;i<dim+1;i++){
+                        v[i]=generaCasuale(1,dim);
+                    }
+                    break;
+                case QUASI_ORDINATO:
+                    v[0]=0;
+                    for(i=1;i<dim+1/2;i++){
+                        v[i]=i+1;
+                    }
+                    for(j=i;j<dim+1;j++){
+                        v[j]=generaCasuale(10,20);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
+    return v;
+}
+
+
+int generaCasuale(int min, int max) {
+    int r;
+    r = min + rand() % (max - min + 1);
+    return r;
 }
